4ad47335401af3709b6ccba7d815bce8dd8d1b0e
Diego <diego@odd.co>
2022-04-23 22:17:23 -0400

    feat: project library v0.0.1

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_namespace_packages, setup
+
+
+setup(
+    name="ml_heroku_fastapi",
+    packages=find_namespace_packages(),
+    version="0.0.1",
+    description="Library of ml-heroku-fastapi project.",
+    author="Diego Canales",
+    python_requires=">=3.8",
+)
